8c4763ca8d0d8384da1147f058e3bb52d1584436
Pankaj-automation <qajcsoftwaresolution.com>
2025-04-11 16:53:16 +0530

    new

--- src/test/java/TC_For_Signup/SignUpTest.java
+++ src/test/java/TC_For_Signup/SignUpTest.java
@@ -15,36 +15,41 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 
 public class SignUpTest {
-	WebDriver driver;
+	 WebDriver driver;
 
-	@Test
-	public void Openbrowser() throws InterruptedException {
-		ChromeOptions options = new ChromeOptions();
+	    @BeforeClass
+	    public void setUp() {
 
-		  // 💡 UNIQUE user data dir
-		String uniqueDir = "/tmp/chrome-profile-" + System.currentTimeMillis();
-		options.addArguments("--user-data-dir=" + uniqueDir);
+	        ChromeOptions options = new ChromeOptions();
 
-        String tempUserDataDir = System.getProperty("java.io.tmpdir") + "/chrome-user-data-" + UUID.randomUUID();
-        options.addArguments("--user-data-dir=" + tempUserDataDir);
+	        // Generate a unique user-data-dir for every test run
+	        String userDataDir = System.getProperty("java.io.tmpdir") + "/chrome-profile-" + System.currentTimeMillis();
+	        options.addArguments("--user-data-dir=" + userDataDir);
 
-        // Optional but useful for Jenkins/Linux
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
+	        driver = new ChromeDriver(options);
+	    }
 
-		driver = new ChromeDriver();
+	    @Test
+	    public void Openbrowser() {
+	        driver.get("https://www.google.com");
+	        System.out.println("Page title is: " + driver.getTitle());
+	    }
 
-		driver.manage().window().maximize();
-		Thread.sleep(2000);
-		driver.get("https://www.google.com");
-		driver.quit();
+	    @AfterClass
+	    public void tearDown() {
+	        if (driver != null) {
+	            driver.quit();
+	        }
+	    }
 	}
 
 	// button[normalize-space()='Ok']
-}
+
